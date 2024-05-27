88929a83a1c3a00746c66936f6697131598cb08e
곽윤호 <kwakdm95@gmail.com>
2024-05-27 21:16:56 +0630

    페이지 라우터 생성 다크모드-텍스트 적용: root단계에서

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,22 @@
+// package
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// components
+import Main from "@/components/Main/page";
+import NotFoundPage from "@/components/Fallback/page";
+
+// style sheet
 import "./index.css";
 
 export default function App() {
   return (
-    <div>
-      <p>안녕 월드</p>
+    <div className={"text-foreground"}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
