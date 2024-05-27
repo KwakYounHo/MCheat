5dfdf9edb6a06c5f52fe4f66746f10a56c84131f
곽윤호 <kwakdm95@gmail.com>
2024-05-27 21:16:15 +0630

    서비스의 메인 컬러 설정

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+      },
+    },
   },
   plugins: [],
 };
