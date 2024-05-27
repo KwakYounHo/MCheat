6921afb62f798d7daa0f7c2d20fb9be719d97962
곽윤호 <kwakdm95@gmail.com>
2024-05-27 17:44:01 +0630

    개발환경 세팅

--- front/README.md
+++ front/README.md
@@ -1,11 +1,28 @@
-# MCheat(가제)
+# 🌐 MCheat(가제)
 
 온라인 거래 간 피해 사례 조회 / 등록 서비스
 
-## history
+## 📝 History
 
 > Project Start
 
-2023/05/27
+### 2023/05/27
 
-create repository
+1. create repository
+2. create React-application
+
+<br>
+
+> 개발 환경 설정
+
+### 2023/05/27
+
+1. Typescript Path alias 설정(CRA)
+
+- `craco` 패키지 설치
+
+2. TailwindCSS 설치 및 적용
+
+- `postcss` / `autoprefixer` 패키지 설치
+
+3. 페이지 라우트를 위한 `react-router-dom`패키지 설치
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,26 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>안녕 월드</p>
     </div>
   );
 }
-
-export default App;
