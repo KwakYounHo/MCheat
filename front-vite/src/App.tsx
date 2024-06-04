cb7797f82d95e3c8106ad1b4d63112160d8c4113
곽윤호 <kwakdm95@gmail.com>
2024-06-04 22:26:14 +0630

    페이지 레이아웃

--- front-vite/src/App.tsx
+++ front-vite/src/App.tsx
@@ -1,12 +1,19 @@
 import { ThemeProvider } from "@/components/theme/theme-provider";
 import { Routes, Route } from "react-router-dom";
 
+import Layout from "@/components/layout/layout";
+import Home from "@/app/home/page";
+import NotFound from "@/app/not-found/page";
+
 export default function App() {
   return (
     <ThemeProvider>
-      <Routes>
-        <Route path={"/"} element={<p>Hello wolrd :)</p>} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path={"/"} element={<Home />} />
+          <Route path={"*"} element={<NotFound />} />
+        </Routes>
+      </Layout>
     </ThemeProvider>
   );
 }
