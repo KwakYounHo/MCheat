46fb8937e7cbb3e5753febf382bf242c2e5e5d5c
곽윤호 <kwakdm95@gmail.com>
2024-06-05 17:32:37 +0630

    헤더에 서비스 이름 제거

--- front-vite/src/components/layout/header/header-avatar.tsx
+++ front-vite/src/components/layout/header/header-avatar.tsx
@@ -5,14 +5,11 @@ import LogoFace from "@/assets/img/Logo-face.png";
 
 export default function HeaderAvator() {
   return (
-    <div className={"flex items-center gap-4"}>
-      <Avatar>
-        <Link to={"/"}>
-          <AvatarImage src={LogoFace} />
-          <AvatarFallback>SS</AvatarFallback>
-        </Link>
-      </Avatar>
-      <p className={"hidden lg:block"}>SScammer</p>
-    </div>
+    <Avatar>
+      <Link to={"/"}>
+        <AvatarImage src={LogoFace} />
+        <AvatarFallback>SS</AvatarFallback>
+      </Link>
+    </Avatar>
   );
 }
