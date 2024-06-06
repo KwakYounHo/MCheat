04460898bde8904b3256558ad2e3bee188bfda4b
곽윤호 <kwakdm95@gmail.com>
2024-06-06 16:07:48 +0630

    404페이지 단장 - 이미지 추가(로고 페이스) - CSS파일 모듈화

--- front-vite/src/app/not-found/page.tsx
+++ front-vite/src/app/not-found/page.tsx
@@ -1,26 +1,35 @@
 import { useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
+import LogoFace from "@/assets/img/Logo-face.png";
+
+import style from "./not-found.module.css";
 
 export default function NotFound() {
   const navigate = useNavigate();
 
   return (
     <main>
-      <div
-        className={
-          "w-full h-screen absolute top-0 left-0 bg-background flex flex-col justify-center items-center gap-10"
-        }
-      >
-        <p className={"text-6xl"}>Oppps...</p>
-        <p className={"capitalize text-lg"}>can not found page</p>
-        <Button
-          variant="outline"
-          onClick={() => {
-            navigate(-1);
-          }}
-        >
-          Go back to previous page
-        </Button>
+      <div className={style.mainWrapper}>
+        <img
+          src={LogoFace}
+          alt="logo-img.png"
+          className={"opacity-20 w-[20rem]"}
+        />
+        <div className={style.contentWrapper}>
+          <p className={"text-6xl font-bold"}>Oppps...</p>
+          <p className={"capitalize text-lg font-semibold"}>
+            can not found page
+          </p>
+          <Button
+            variant="outline"
+            onClick={() => {
+              navigate(-1);
+            }}
+            className={"text-lg rounded-md"}
+          >
+            Go back to previous page
+          </Button>
+        </div>
       </div>
     </main>
   );
