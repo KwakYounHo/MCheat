2b0b35bb475b87d5ce0ce4cd161a7326e136554d
곽윤호 <kwakdm95@gmail.com>
2024-06-07 11:05:41 +0630

    검색 - 검색 결과 페이지 생성

---
+++ front-vite/src/app/search/find/page.tsx
@@ -0,0 +1,7 @@
+export default function Find() {
+  return (
+    <>
+      <p>Search result page</p>
+    </>
+  );
+}
--- front-vite/src/app/search/page.tsx
+++ front-vite/src/app/search/page.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import SearchForm from "@/app/search/search-form/search";
 import NotFound from "@/app/not-found/page";
+import Find from "@/app/search/find/page";
 
 export default function Search() {
   return (
     <Routes>
       <Route path={"/"} element={<SearchForm />} />
+      <Route path={"/search/*"} element={<Find />} />
       <Route path={"*"} element={<NotFound />} />
     </Routes>
   );
