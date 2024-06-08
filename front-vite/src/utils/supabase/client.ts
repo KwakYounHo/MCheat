0921b560512ae7f9b23792c3b7c9d5a6c4824e0b
곽윤호 <kwakdm95@gmail.com>
2024-06-08 13:42:45 +0630

    데이터베이스 인스턴스 중복 생성 버그 해결

--- front-vite/src/utils/supabase/client.ts
+++ front-vite/src/utils/supabase/client.ts
@@ -1,12 +1,13 @@
 import { Database } from "@/lib/supabase";
 import { createClient } from "@supabase/supabase-js";
 
-export const options = {
+const options = {
   URL: import.meta.env.VITE_DATABASE_URL,
   KEY: import.meta.env.VITE_DATABASE_ANON_KEY,
 };
 
-export default function (url: string, key: string) {
-  const database = createClient<Database>(url, key);
-  return database;
-}
+const client = createClient<Database>(options.URL, options.KEY);
+
+const supabase = () => client;
+
+export default supabase;
