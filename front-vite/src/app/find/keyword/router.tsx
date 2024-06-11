34e2d04ae2f522734d0ebc2a4700d802f474aa53
곽윤호 <kwakdm95@gmail.com>
2024-06-11 14:41:23 +0630

    디테일 페이지 삭제 -> 결과 페이지에서 클릭 했을 때 디테일이 보이도록 변경

--- front-vite/src/app/find/keyword/router.tsx
+++ front-vite/src/app/find/keyword/router.tsx
@@ -5,7 +5,6 @@ import { ResultLengthContextProvider } from "./context/result-length";
 
 import Keyword from "./page";
 import NotFound from "@/app/not-found/page";
-import Detail from "./detail/page";
 
 export default function KeywordRouter() {
   return (
@@ -13,7 +12,6 @@ export default function KeywordRouter() {
       <ResultLengthContextProvider>
         <Routes>
           <Route path={"/"} element={<Keyword />} />
-          <Route path={"/:id"} element={<Detail />} />
           <Route path={"*"} element={<NotFound />} />
         </Routes>
       </ResultLengthContextProvider>
