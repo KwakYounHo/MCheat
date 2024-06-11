258d31842a041b15d0ee84cfc05cbcc4ab955fc9
곽윤호 <kwakdm95@gmail.com>
2024-06-11 15:33:10 +0630

    피해 사례 세부내용 팝오버 컴포넌트 적용

--- front-vite/src/app/find/keyword/card01/table/table-data.tsx
+++ front-vite/src/app/find/keyword/card01/table/table-data.tsx
@@ -3,8 +3,8 @@ import { useResultLengthContext } from "@/app/find/keyword/context/result-length
 import database from "@/utils/supabase/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import timeCal from "@/utils/timezoneCalculator";
 
 // context
 import { useKeywordContext } from "@/app/find/keyword/context/keyword";
@@ -12,6 +12,11 @@ import { useKeywordContext } from "@/app/find/keyword/context/keyword";
 // ui
 import { TableRow, TableCell } from "@/components/ui/table";
 import { Plus } from "lucide-react";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@/components/ui/popover";
 
 import style from "@/app/find/keyword/keyword.module.css";
 
@@ -19,7 +24,6 @@ import style from "@/app/find/keyword/keyword.module.css";
 export default function TableData() {
   const { setResultLength } = useResultLengthContext();
   const { keyword } = useKeywordContext();
-  const navigate = useNavigate();
 
   const queryKey = [keyword];
   const queryFn = async () => {
@@ -49,18 +53,38 @@ export default function TableData() {
             return (
               // case data list
               e.detail ? (
-                <TableRow
-                  key={e.sccamer_id}
-                  onClick={() => {
-                    navigate(`/find/${keyword}/${e.sccamer_id}`);
-                  }}
-                  className={"hover:bg-destructive/10 hover:cursor-pointer"}
-                >
-                  <TableCell>{e.name}</TableCell>
-                  <TableCell>{e.bank_account}</TableCell>
-                  <TableCell>{e.mobile_number}</TableCell>
-                  <TableCell>{e.place_of_issue_occur}</TableCell>
-                </TableRow>
+                <Popover key={e.sccamer_id}>
+                  <PopoverTrigger asChild>
+                    <TableRow
+                      className={
+                        "bg-destructive/10 hover:bg-destructive/50 hover:cursor-pointer"
+                      }
+                    >
+                      <TableCell>{e.name}</TableCell>
+                      <TableCell>{e.bank_account}</TableCell>
+                      <TableCell>{e.mobile_number}</TableCell>
+                      <TableCell>{e.place_of_issue_occur}</TableCell>
+                    </TableRow>
+                  </PopoverTrigger>
+                  <PopoverContent
+                    className={
+                      "w-full max-w-[80vw] h-full max-h-[30vh] overflow-auto flex flex-col gap-4"
+                    }
+                  >
+                    <div>
+                      <p className={"capitalize text-lg"}>detail</p>
+                      <p className={"text-muted-foreground text-sm"}>
+                        Scammed case details entered by the user
+                      </p>
+                    </div>
+                    <div className={"whitespace-pre-wrap text-sm"}>
+                      {e.detail}
+                    </div>
+                    <div className={"text-muted-foreground text-xs"}>
+                      {timeCal.toMy(e.entered_at!)}
+                    </div>
+                  </PopoverContent>
+                </Popover>
               ) : (
                 <TableRow key={e.sccamer_id}>
                   <TableCell>{e.name}</TableCell>
